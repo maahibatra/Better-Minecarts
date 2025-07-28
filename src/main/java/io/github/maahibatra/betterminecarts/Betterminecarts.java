300fe45059d31875e1c8dbaadda1d73de61d2acd
Maahi Batra <maahi.batra@gmail.com>
2025-07-28 20:43:14 +0530

    base setup

---
+++ src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
@@ -0,0 +1,10 @@
+package io.github.maahibatra.betterminecarts;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Betterminecarts implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/maahibatra/betterminecarts/client/BetterminecartsClient.java
@@ -0,0 +1,10 @@
+package io.github.maahibatra.betterminecarts.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterminecartsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
