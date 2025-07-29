3a90980679baf123417e75902f5f87efab369cc6
Maahi Batra <maahi.batra@gmail.com>
2025-07-29 17:54:41 +0530

    link minecarts only on rails and one block apart

--- src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
+++ src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
@@ -3,14 +3,19 @@ package io.github.maahibatra.betterminecarts;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.block.RailBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.entity.vehicle.MinecartEntity;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.Block;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.UUID;
 
 public class Betterminecarts implements ModInitializer {
@@ -24,19 +29,36 @@ public class Betterminecarts implements ModInitializer {
         UseEntityCallback.EVENT.register((playerEntity, world, hand, entity, entityHitResult) -> {
             if(!world.isClient() && entity.getType() == EntityType.MINECART && playerEntity.getMainHandStack().getItem() == Items.CHAIN) {
 
+                BlockPos pos = entity.getBlockPos();
+                Block block = world.getBlockState(pos).getBlock();
+
+                if (!(block instanceof RailBlock)) {
+                    playerEntity.sendMessage(Text.literal("Minecart must be on rails."), false);
+                    return ActionResult.SUCCESS;
+                }
+
                 if(map.size() == 0) {
                     playerEntity.sendMessage(Text.literal("You clicked the first minecart with a chain!"), false);
                     map.put(playerEntity.getUuid(), (MinecartEntity) entity);
                 } else {
                     AbstractMinecartEntity storedCart = map.get(playerEntity.getUuid());
 
-                    if (storedCart == null || !storedCart.isAlive() || storedCart.isRemoved()) {
-                        playerEntity.sendMessage(Text.literal("The first minecart is no longer valid. This is your first minecart, now."), false);
+                    if (storedCart == null || !storedCart.isAlive() || storedCart.isRemoved() || storedCart == entity) {
+                        playerEntity.sendMessage(Text.literal("The first minecart is invalid. This is your first minecart, now."), false);
                         map.put(playerEntity.getUuid(), (MinecartEntity) entity);
                     } else {
-                        playerEntity.sendMessage(Text.literal("You clicked the second minecart and linked it!"), false);
-                        playerEntity.getMainHandStack().decrement(1);
-                        map.remove(playerEntity.getUuid());
+                        BlockPos pos1 = storedCart.getBlockPos();
+                        BlockPos pos2 = entity.getBlockPos();
+
+                        if(pos1.getManhattanDistance(pos2) != 2) {
+                            playerEntity.sendMessage(Text.literal("Minecarts must be exactly one block apart."), false);
+                            map.remove(playerEntity.getUuid());
+                            return ActionResult.SUCCESS;
+                        } else {
+                            playerEntity.sendMessage(Text.literal("You clicked the second minecart and linked it!"), false);
+                            playerEntity.getMainHandStack().decrement(1);
+                            map.remove(playerEntity.getUuid());
+                        }
                     }
                 }
             }
