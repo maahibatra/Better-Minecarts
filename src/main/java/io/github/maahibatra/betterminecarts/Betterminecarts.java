1f7c3e1fcc771c22058dcdd5a11955bff7fb31a4
Maahi Batra <maahi.batra@gmail.com>
2025-07-29 13:21:19 +0530

    its got a bit of logic now

--- src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
+++ src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
@@ -1,12 +1,18 @@
 package io.github.maahibatra.betterminecarts;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.entity.vehicle.MinecartEntity;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+
+import javax.swing.*;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class Betterminecarts implements ModInitializer {
 
@@ -14,9 +20,18 @@ public class Betterminecarts implements ModInitializer {
     public void onInitialize() {
         System.out.println("BETTER MINECARTS LOADED SUCCESSFULLY.");
 
+        HashMap<UUID, AbstractMinecartEntity> map = new HashMap<>();
+
         UseEntityCallback.EVENT.register((playerEntity, world, hand, entity, entityHitResult) -> {
             if(!world.isClient() && entity.getType() == EntityType.MINECART && playerEntity.getMainHandStack().getItem() == Items.CHAIN) {
-                playerEntity.sendMessage(Text.literal("You clicked a minecart with a chain!"), false);
+                if(map.size() == 0) {
+                    playerEntity.sendMessage(Text.literal("You clicked the first minecart with a chain!"), false);
+                    playerEntity.getMainHandStack().decrement(1);
+                    map.put(playerEntity.getUuid(), (MinecartEntity) entity);
+                } else {
+                    playerEntity.sendMessage(Text.literal("You clicked the second minecart and linked it!"), false);
+                    map.remove(playerEntity.getUuid());
+                }
             }
             return ActionResult.SUCCESS;
         });
