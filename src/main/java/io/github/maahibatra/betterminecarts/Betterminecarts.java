2e33f30881c9b06d3f635bdcbb120f09fd6b1a22
Maahi Batra <maahi.batra@gmail.com>
2025-07-29 12:45:06 +0530

    only sends a message on clicking a minecart with a chain

--- src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
+++ src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
@@ -2,9 +2,11 @@ package io.github.maahibatra.betterminecarts;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
+import java.util.HashMap;
 
 public class Betterminecarts implements ModInitializer {
 
@@ -13,8 +15,8 @@ public class Betterminecarts implements ModInitializer {
         System.out.println("BETTER MINECARTS LOADED SUCCESSFULLY.");
 
         UseEntityCallback.EVENT.register((playerEntity, world, hand, entity, entityHitResult) -> {
-            if(!world.isClient()) {
-                playerEntity.sendMessage(Text.literal("Entity clicked: " + entity.getType()), false);
+            if(!world.isClient() && entity.getType() == EntityType.MINECART && playerEntity.getMainHandStack().getItem() == Items.CHAIN) {
+                playerEntity.sendMessage(Text.literal("You clicked a minecart with a chain!"), false);
             }
             return ActionResult.SUCCESS;
         });
