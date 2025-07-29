503894a9531b56fcf9b9f07119167bc4b9a02d12
Maahi Batra <maahi.batra@gmail.com>
2025-07-29 12:08:09 +0530

    you get a message when you click an entity. phenomenol, i know. this took too long istg

--- src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
+++ src/main/java/io/github/maahibatra/betterminecarts/Betterminecarts.java
@@ -1,10 +1,22 @@
 package io.github.maahibatra.betterminecarts;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 
 public class Betterminecarts implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        System.out.println("BETTER MINECARTS LOADED SUCCESSFULLY.");
+
+        UseEntityCallback.EVENT.register((playerEntity, world, hand, entity, entityHitResult) -> {
+            if(!world.isClient()) {
+                playerEntity.sendMessage(Text.literal("Entity clicked: " + entity.getType()), false);
+            }
+            return ActionResult.SUCCESS;
+        });
     }
 }
